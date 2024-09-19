0068eb87ab26a30adda7c7531f37339fa518ac94
shirayki <wayhq1314@gmail.com>
2024-09-19 15:29:03 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -94,7 +94,7 @@ var tianliGPT = {
     // }
 
     try {
-      const response = await fetch('替换你自己的', {
+      const response = await fetch('https://ai.ntb.us.kg/?token=200', {
         signal: signal,
         method: "POST",
         headers: {
